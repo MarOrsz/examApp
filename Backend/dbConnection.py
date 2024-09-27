4ad4c4046e19eedd4b322f067683d4fff4e2c82e
Marcin Orszulak <marcin.orszulak@primetals.com>
2024-09-27 17:52:40 -0300

    Connection to online-exams DB

--- Backend/dbConnection.py
+++ Backend/dbConnection.py
@@ -2,8 +2,13 @@ from sqlalchemy import create_engine, Table, MetaData, Column, String, DateTime,
 
 from sqlalchemy.ext.automap import automap_base
 
-
 engine = create_engine("mssql+pymssql://prdb:a423pa@localhost:1433/PRDB_NovelisPindaHMA")
+
+db_url = 'localhost:1433'
+db_name = 'online-exam'
+db_user = 'exam'
+db_password = 'pass'
+engine = create_engine(f'mssql+pymssql://{db_user}:{db_password}@{db_url}/{db_name}')
 
 
 Base = automap_base()
@@ -14,16 +19,17 @@ Base.prepare(autoload_with=engine)
 for cl in Base.classes :
     print (Base.classes)
 
-pdi = Base.classes.PRIMARYDATA
 
-stmt = select(pdi.slabThickness).where(pdi.meId > 0)
+exams = Base.classes.exams
+
+stmt = select(exams.id)
+
+# pdi = Base.classes.PRIMARYDATA
+
+# stmt = select(pdi.slabThickness).where(pdi.meId > 0)
 
 connection = engine.connect()
-
 results = connection.execute(stmt).fetchall()
 
 for res in results:
     print(res)
-
-
-
